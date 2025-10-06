a5d1f7acac9ae81a9fef445536de2d31d1307a73
oceanyu1 <81434548+oceanyu1@users.noreply.github.com>
2025-10-06 18:58:06 -0400

    Remove timeout temporarily

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import type { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080/api",
-    timeout: 10000,
+    //timeout: 10000,
     withCredentials: true,
 });
 
