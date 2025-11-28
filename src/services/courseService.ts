07cd1873c7566298b308d066e4c08b25d122e5b5
oceanyu1 <81434548+oceanyu1@users.noreply.github.com>
2025-11-27 19:45:13 -0500

    Change home page course list to default to average rating descending in courseService.ts, Home.tsx, CourseList.tsx

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -30,7 +30,7 @@ interface Course {
 export const courseService = {
     // Search courses with query, optional sorting, and department filter
     searchCourses: (query: string | undefined, 
-                    sortBy: string = 'courseNumber_asc', 
+                    sortBy: string = 'averageRating_desc', 
                     departmentCode: string | undefined,
                     page: number = 0,
                     size: number = 60) => {
